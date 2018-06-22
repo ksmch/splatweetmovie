56d3035eb79e2e6ac3526662362540513d4c0a20
masashi <frantic.cranberry495@gmail.com>
2018-06-23 01:44:27 +0900

    初版

---
+++ sampleapp.js
@@ -0,0 +1,40 @@
+var http = require('http');
+var fs = require('fs');
+var ejs = require('ejs');
+var mysql = require('mysql');
+
+var ejs_hello = fs.readFileSync("./hello.ejs", 'utf-8');
+var ejs_tweetlist = fs.readFileSync("./tweetlist.ejs", 'utf-8');
+
+var connection = mysql.createConnection({
+  host:'us-cdbr-iron-east-04.cleardb.net',
+  user:'bd4dbe8beb9637',
+  password:'2c18df4b',
+  database:'heroku_a70d299fbf5da8d',
+  port:3306
+});
+connection.connect();
+
+const PORT = process.env.PORT || 5000
+var server = http.createServer();
+server.on('request', doRequest);
+server.listen(PORT, () => console.log(`Listening on ${ PORT }`));
+console.log('Server running!');
+
+// リクエストの処理
+function doRequest(req, res) {
+  connection.query('select * from tweet', function(error,results,fields){
+    if (error) {
+      console.log(error);
+    }
+
+    var rend_hello = ejs.render(ejs_hello,{
+      tweetlist:ejs.render(ejs_tweetlist,{
+        tweets:results
+      })
+    });
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(rend_hello);
+    res.end();
+  });
+}
